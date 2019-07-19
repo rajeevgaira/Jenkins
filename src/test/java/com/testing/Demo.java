5e82c7c978845d021339053070557daaf57bbbaf
rajeevgaira <38223956+rajeevgaira@users.noreply.github.com>
2019-07-19 19:38:39 +0530

    Add files via upload

--- src/test/java/com/testing/Demo.java
+++ src/test/java/com/testing/Demo.java
@@ -1,5 +1,6 @@
 package com.testing;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -11,5 +12,8 @@ public class Demo {
 	public void test1() {
 		 WebDriver d = new SafariDriver();
 		 d.get("https://www.google.com");
+		 d.manage().window().maximize();
+		 d.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("M S Dhoni");
+		 d.findElement(By.xpath("(//input[@value='Google Search'])[2]")).click();
 	}
 }
