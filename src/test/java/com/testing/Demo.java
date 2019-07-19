c9586962cc229a9ad6f80c1cf24a46849f09d46b
rajeevgaira <38223956+rajeevgaira@users.noreply.github.com>
2019-07-19 19:30:22 +0530

    Add files via upload

--- src/test/java/com/testing/Demo.java
+++ src/test/java/com/testing/Demo.java
@@ -1,11 +1,14 @@
 package com.testing;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Test;
 
 public class Demo {
 
-	public static void main(String[] args) {
+	@Test
+	public void test1() {
 		 WebDriver d = new SafariDriver();
 		 d.get("https://www.google.com");
 	}
